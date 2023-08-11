96c31e31c1ae6f27ec865ef400b5dd45fa3314f3
chokoo7 <adanzamudio777@gmail.com>
2023-08-11 10:49:04 -0600

    Proyecto Final

--- funcionPDF.py
+++ funcionPDF.py
@@ -24,63 +24,67 @@ def generarPDF(listaNombre, listaEdades):
     c = canvas.Canvas(nombreArchivo,  pagesize=(650,400))
     rutaFuente= "c:/Windows/Fonts/KUNSTLER.TTF"
     #Aqui va el for
-    for i in range(len(listaNombre)):
-        pdfmetrics.registerFont(TTFont('Mifuente',rutaFuente))
-        c.setFont("Mifuente",12)    
-        xInicial = 200
-        yInicial = 300
-        
-        c.drawImage(rutaImg, 400, 270, width=150, height=70)
-        c.drawImage(rutaImg2, 80, 227, width=210, height=130) 
+    if (len(listaNombre)<1):
+        print("NO hay datos")
+    else:
 
-        color = (0,0,0)
-        c.setFillColor(color)
-    
-        c.rect(0,350,700,55,fill=True)
-        c.setFillColorRGB(*color)
-
-        c.setFillColorRGB(1, 1, 1)
-
-        c.setFont("Helvetica-Bold", 20)
-        c.drawString(xInicial - 30 ,yInicial + 60,"Cédula de Identificación Fiscal")
-    
-
-        c.setFillColorRGB(0,0,0)
-        c.setFont("Helvetica-Bold", 20)
-        c.drawString(xInicial+150, yInicial-50,"RFC CONTRIBUYENTE 000")
-
-        c.setFont("Helvetica", 12)
-        c.drawString(xInicial+150, yInicial-70,"Registro Federal de Contribuyentes ")
-
-        c.setFont("Helvetica-Bold", 13)
-        c.drawString(xInicial+150,yInicial-140,"NOMBRE CONTRIBUYENTE")
-        c.drawImage(nombreQR,xInicial - 150 ,yInicial-270,270,230)
-
-        c.setFont("Helvetica", 12)
-        c.drawString(350,130,listaNombre[i])
-
-        c.setFont("Helvetica-Bold", 13)
-        c.drawString(xInicial+150,yInicial-220,"DENOMINACION O RAZON SOCIAL")
-
-       
-        #yInicial = yInicial - 160
-       
-        
-        #c.drawString(350,50,listaEdades[i])
-        
-        #c.drawString(xInicial+160, yInicial-240,"odaaaaa")
-
-
-        # for i in range(len(listaNombre)):
+        for i in range(len(listaNombre)):
+            pdfmetrics.registerFont(TTFont('Mifuente',rutaFuente))
+            c.setFont("Mifuente",12)    
+            xInicial = 200
+            yInicial = 300
             
-        #     c.setFont("Helvetica", 9)
-        #     #yInicial = yInicial - 160
-        #     #c.drawString(xInicial,yInicial,listaEdades[i])
-        #     c.drawString(xInicial + 160 ,yInicial-160,listaNombre[i])
-        #     c.drawString(xInicial+160, yInicial-240,"odaaaaa")
-        c.showPage()
-    c.save()
-    print("Reporte generado")
+            c.drawImage(rutaImg, 400, 270, width=150, height=70)
+            c.drawImage(rutaImg2, 80, 227, width=210, height=130) 
+
+            color = (0,0,0)
+            c.setFillColor(color)
+        
+            c.rect(0,350,700,55,fill=True)
+            c.setFillColorRGB(*color)
+
+            c.setFillColorRGB(1, 1, 1)
+
+            c.setFont("Helvetica-Bold", 20)
+            c.drawString(xInicial - 30 ,yInicial + 60,"Cédula de Identificación Fiscal")
+        
+
+            c.setFillColorRGB(0,0,0)
+            c.setFont("Helvetica-Bold", 20)
+            c.drawString(xInicial+150, yInicial-50,"RFC CONTRIBUYENTE 00"+str(i+1))
+
+            c.setFont("Helvetica", 12)
+            c.drawString(xInicial+150, yInicial-70,"Registro Federal de Contribuyentes ")
+
+            c.setFont("Helvetica-Bold", 13)
+            c.drawString(xInicial+150,yInicial-140,"NOMBRE CONTRIBUYENTE")
+            c.drawImage(nombreQR,xInicial - 150 ,yInicial-270,270,230)
+
+            c.setFont("Helvetica", 12)
+            c.drawString(350,130,listaNombre[i])
+
+            c.setFont("Helvetica-Bold", 13)
+            c.drawString(xInicial+150,yInicial-220,"DENOMINACION O RAZON SOCIAL")
+
+        
+            #yInicial = yInicial - 160
+        
+            
+            #c.drawString(350,50,listaEdades[i])
+            
+            #c.drawString(xInicial+160, yInicial-240,"odaaaaa")
+
+
+            # for i in range(len(listaNombre)):
+                
+            #     c.setFont("Helvetica", 9)
+            #     #yInicial = yInicial - 160
+            #     #c.drawString(xInicial,yInicial,listaEdades[i])
+            #     c.drawString(xInicial + 160 ,yInicial-160,listaNombre[i])
+            #     c.drawString(xInicial+160, yInicial-240,"odaaaaa")
+            c.showPage()
+        c.save()
+        print("Reporte generado")
 
 
 def generarComprobante(numC, nomClientes,motivo,RFC):
